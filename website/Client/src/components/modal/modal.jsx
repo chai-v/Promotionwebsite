caa6ed8a36ce742dd100511f574910194eeb976a
chai-v <chaitanya.vengali@gmail.com>
2024-01-25 22:18:06 +0530

    republic day sale

--- website/Client/src/components/modal/modal.jsx
+++ website/Client/src/components/modal/modal.jsx
@@ -13,9 +13,9 @@ const Modal = ({onClose}) => {
             <div className='mt-10 flex flex-col text-white'>
                 <button onClick={onClose} className='place-self-end'><ion-icon name="close-outline"></ion-icon></button>
                 <div className='bg-purple-900 rounded-xl px-4 py-4 flex-col gap-5 items-center mx-3'>
-                    <h1 className='text-2xl font-extrabold'>Passes are out!</h1><br/>
-                    <p className='text-2xl font-bold max-w-md text-center'>Passes for Gitamites and Non-Gitamites are now available</p><br/>
-                    <p className='text-l max-w-md text-center'>Note: Use GITAM mail</p>
+                    <h1 className='text-2xl font-extrabold'>Republic day sale!</h1><br/>
+                    <p className='text-2xl font-bold max-w-md text-center'>Passes for Gitamites are now available at 700/- on January 26th</p><br/>
+                    <p className='text-l max-w-md text-center'>Note: The sale does not apply to Non Gitamite passes</p>
                     <button id="buy" className='bg-white mt-4 font-extrabold rounded-md flex items-center justify-center'><a className='text-purple-900' href="https://linktr.ee/pramanahyd">Buy now</a></button>
                 </div>
             </div>
